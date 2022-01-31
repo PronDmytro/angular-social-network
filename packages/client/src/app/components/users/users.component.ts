0c7adc619a54ee4ae1f0be2082ba1f212d6ee9e9
PronDmytro <dmytropron5838@gmail.com>
2022-01-31 22:37:36 +0200

    feat(client): add users search

--- packages/client/src/app/components/users/users.component.ts
+++ packages/client/src/app/components/users/users.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User, UserDataService } from '../../services/user-data.service';
+import { map, startWith } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -9,6 +12,8 @@ import { User, UserDataService } from '../../services/user-data.service';
 export class UsersComponent implements OnInit {
 
   public users: User[];
+  public stateCtrl = new FormControl();
+  public filteredUsers: Observable<User[]>;
 
   public constructor(
     private userDataService: UserDataService,
@@ -18,6 +23,15 @@ export class UsersComponent implements OnInit {
   public async ngOnInit(): Promise<void> {
     this.users = await this.userDataService.getAllUsers();
     await this.userDataService.getUserData(true);
+    this.filteredUsers = this.stateCtrl.valueChanges.pipe(
+      startWith(''),
+      map((state) => (state ? this._filterStates(state) : this.users.slice())),
+    );
+  }
+
+  private _filterStates(value: string): User[] {
+    const filterValue = value.toLowerCase();
+    return this.users.filter((user) => (user.name.toLowerCase() + ' ' + user.surname.toLowerCase()).includes(filterValue));
   }
 
 }
--- packages/client/src/app/components/users/users.module.ts
+++ packages/client/src/app/components/users/users.module.ts
@@ -11,6 +11,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 import { EducationInfoComponent } from './user-info/education-info/education-info.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -28,6 +32,10 @@ import { EducationInfoComponent } from './user-info/education-info/education-inf
     MatButtonModule,
     MatIconModule,
     TranslateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
   ],
   exports: [
     UsersComponent,
